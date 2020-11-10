1d22067cc9bad8f1afb36e0ebeced8be17cd3097
Antonio Estevam da Silva Filho(Jhonny) <47607852+Jhonny3x4@users.noreply.github.com>
2020-11-10 11:08:38 -0300

    script db

--- html_php/enviar_quest.php
+++ html_php/enviar_quest.php
@@ -7,10 +7,10 @@ $pergunta=$_POST["pergunta"];
 $alt1=$_POST["alternativa1"];
 
 $base->query("INSERT INTO `inqueetesdb`.`question` (`idquestion`, `question_body`, `question_status`, `id_user_fk`) VALUES ('".$id_alt."', '".$pergunta."','1','".$usu."')"); 
-enviar($id_alt,$alt1);
+$base->query("INSERT INTO option (`option_body`, `id_question_fk`) VALUES ('".$alt1."', '".$id_alt."')"); 
 
 #INSERT INTO `inqueetesdb`.`question` (`idquestion`, `question_body`, `question_status`, `id_user_fk`) VALUES ('AVS004', 'bbb', '1', '1');
-
+/*
 if($_POST["alternativa2"]){
     $alt2=$_POST["alternativa2"];
     enviar($id_alt,$alt2);
@@ -37,13 +37,13 @@ if($_POST["alternativa6"]){
     $alt6=$_POST["alternativa6"]; 
     enviar($id_alt,$alt6);
 }else{}
+*/
 
-
-
+/*
 function enviar($id_alt,$alt1){
     $base->query("INSERT INTO option (`option_body`, `id_question_fk`) VALUES ('".$alt1."', '".$id_alt."')");      
 
-}
+}*/
 
 
 
